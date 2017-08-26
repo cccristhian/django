ccbd2bb7fbfe33cb3e5f65975ce7f339b27c500a
calel <cccristhian@mesoamericana.edu.gt>
2017-08-25 21:19:44 -0600

    se despliegan los datos de la tabla

--- bolg/views.py
+++ bolg/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Publicar
+
 
 def listar_publicacion(request):
+    publi = Publicar.objects.filter(fecha_publica__lte=timezone.now()).order_by('fecha_publica')
      #se busca en la carpeta de template el blog
-        return render(request, 'blog/listar_publicacion.html', {})
+    return render(request, 'blog/listar_publicacion.html', {'publi':publi})
