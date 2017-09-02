4ea61e4796902227cf22ea8995c483594e43f8a6
calel <cccristhian@mesoamericana.edu.gt>
2017-09-01 20:44:33 -0600

    ingresando nuevos cambios

--- bolg/urls.py
+++ bolg/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 #cuando la linea este vasia nos envia a la siguiente vista
         url(r'^$', views.listar_publicacion),
+        url(r'^postear/(?P<pk>[0-9]+)/$', views.detalle_pub,name='postear'),
 ]
